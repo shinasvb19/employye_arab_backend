816e67325225847b02d24162220c55b4fbfaeb0c
shinas vb <myofficial.shinas@gmail.com>
2023-07-09 18:50:20 +0530

    sm

--- controllers/subManagerController.js
+++ controllers/subManagerController.js
@@ -2,13 +2,15 @@ const managerSchema = require('../models/managerModel')
 const storeSchema = require('../models/storeModel')
 const employeeSchema = require('../models/employeeModel')
 const subManagersSchema = require('../models/subManagersModel')
+const mongoose = require('mongoose')
 exports.createManager = async (req, res) => {
     try {
         const newSubManager = new subManagersSchema(req.body.name);
         await newSubManager.save();
-        const updateStore = await storeSchema.findOneAndUpdate(req.body.id, { $set: { manager: req.body.empId } })
+        const submanagerId = new mongoose.Types.ObjectId(newSubManager._id)
+        const updateManager = await managerSchema.findOneAndUpdate(req.body.managerId, { $push: { subManagers: submanagerId } })
         const updateEmployee = await employeeSchema.findOneAndUpdate(req.body.empId, { $set: { role: 'manager' } })
-        res.status(200).json(newManager)
+        res.status(200).json(newSubManager)
     } catch (error) {
         res.status(500).send({ errMsg: "Internal server error" });
         console.log(error);
