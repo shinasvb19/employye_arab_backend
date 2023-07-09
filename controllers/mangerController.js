2324c90775486270ec2e87e44bc6b3d8da962fde
shinas vb <myofficial.shinas@gmail.com>
2023-07-09 15:45:07 +0530

    createManagerCompleated

--- controllers/mangerController.js
+++ controllers/mangerController.js
@@ -1,9 +1,12 @@
 const managerSchema = require('../models/managerModel')
+const storeSchema = require('../models/storeModel')
+const employeeSchema = require('../models/employee')
 exports.createManager = async (req, res) => {
     try {
-        const newManager = new managerSchema(req.body);
+        const newManager = new managerSchema(req.body.name);
         await newManager.save();
-
+        const updateStore = await storeSchema.findOneAndUpdate(req.body.id, { $set: { manager: req.body.empId } })
+        const updateEmployee = await employeeSchema.findOneAndUpdate(req.body.empId, { $set: { role: 'manager' } })
         res.status(200).json(newManager)
     } catch (error) {
         res.status(500).send({ errMsg: "Internal server error" });
