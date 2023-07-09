c85bc3d3645569e20f43aa2fd967f4b481c5c5eb
shinas vb <myofficial.shinas@gmail.com>
2023-07-09 19:29:04 +0530

    mangerRouteAdded

--- controllers/mangerController.js
+++ controllers/mangerController.js
@@ -3,7 +3,7 @@ const storeSchema = require('../models/storeModel')
 const employeeSchema = require('../models/employeeModel')
 exports.createManager = async (req, res) => {
     try {
-        const newManager = new managerSchema(req.body);
+        const newManager = new managerSchema({ name: req.body.name, employeeCode: req.body.empId });
         await newManager.save();
         const updateStore = await storeSchema.findOneAndUpdate(req.body.id, { $set: { manager: req.body.empId } })
         const updateEmployee = await employeeSchema.findOneAndUpdate(req.body.empId, { $set: { role: 'manager' } })
