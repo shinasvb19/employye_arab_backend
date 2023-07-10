2639607c6eec4fa32fe3ca8215870d3d85665c97
shinas vb <myofficial.shinas@gmail.com>
2023-07-10 22:47:57 +0530

    small_change_in_id

--- routers/workerRouter.js
+++ routers/workerRouter.js
@@ -3,8 +3,8 @@ const {
     createWorker, getManager, addCoworker, getWorkers
 } = require("../controllers/workerController");
 const router = express.Router();
-router.get('/get-workers/:id', getWorkers)
 router.post("/add-worker", createWorker);
 router.get("/get-asst-manager", getManager)
 router.post("/add-co-worker", addCoworker)
+router.get('/get-workers/:id', getWorkers)
 module.exports = router;
