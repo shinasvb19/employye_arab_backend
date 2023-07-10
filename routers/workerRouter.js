5d0b09cad8b5d449e7330282b33c8328046a38e1
shinas vb <myofficial.shinas@gmail.com>
2023-07-10 23:07:23 +0530

    ad

--- routers/workerRouter.js
+++ routers/workerRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
-    createWorker, getManager, addCoworker, getWorkers
+    createWorker, getManager, addCoworker, getWorkers, lookUpEmployee
 } = require("../controllers/workerController");
 const router = express.Router();
 router.post("/add-worker", createWorker);
 router.get("/get-asst-manager", getManager)
 router.post("/add-co-worker", addCoworker)
+
 router.get('/get-workers/:id', getWorkers)
+router.get('/get-co-workers/:1d', lookUpEmployee)
 module.exports = router;
